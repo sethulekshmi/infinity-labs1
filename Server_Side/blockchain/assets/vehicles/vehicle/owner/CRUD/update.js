1d34a16c7c2055d1b39319cc6aa56591a26f6da8
sethulekshmi <sethulekshmi777@gmail.com>
2017-03-07 23:12:14 +0530

    Update update.js

--- Server_Side/blockchain/assets/vehicles/vehicle/owner/CRUD/update.js
+++ Server_Side/blockchain/assets/vehicles/vehicle/owner/CRUD/update.js
@@ -3,5 +3,5 @@
 let update = require(__dirname+'/../../property/update');
 
 exports.update = function(req, res, next, usersToSecurityContext) {
-    return update(req, res, next, usersToSecurityContext, 'location');
+    return update(req, res, next, usersToSecurityContext, 'owner');
 };
