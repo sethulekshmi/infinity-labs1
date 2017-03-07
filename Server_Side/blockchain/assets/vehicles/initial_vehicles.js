f016f196c5c5d8a992ad9a9667542349bc45ba85
sethulekshmi <sethulekshmi777@gmail.com>
2017-03-07 23:23:40 +0530

    Update initial_vehicles.js

--- Server_Side/blockchain/assets/vehicles/initial_vehicles.js
+++ Server_Side/blockchain/assets/vehicles/initial_vehicles.js
@@ -2,116 +2,133 @@
 let simple_scenario = {
     'cars': [
         {
-            'VIN': '287437467447767',
-            'Make': 'Toyota',
-            'Model': 'Auris',
-            'Colour': 'Blue',
-            'Reg': 'LM16 YHU',
-            'Owners': ['DVLA', 'Toyota', 'Beechvale Group', 'LeaseCan']
-        },
-        {
-            'VIN': '549523556856725',
-            'Make': 'Jaguar',
-            'Model': 'F-Type',
-            'Colour': 'Red',
-            'Reg': 'HE16 WDZ',
-            'Owners': ['DVLA', 'Jaguar Land Rover', 'Beechvale Group']
-        },
-        {
-            'VIN': '880352730316924',
-            'Make': 'Alfa Romeo',
-            'Model': 'MiTo',
-            'Colour': 'Blue',
-            'Reg': 'NL65 DTU',
-            'Owners': ['DVLA', 'Alfa Romeo']
-        }
-    ]
+            
+			"Diamondat": "2.0",
+			"Cut": "Point",
+			"Colour": "Blue",
+			"Clarity": "VVS1",
+			"Location":"abc" ,
+			
+			
+			"Owners": ["Kollur","soham_industrial_diamonds","Beon_Group","Shah","Ajay_Gosh","Crayon_Bros_Ltd","Adora","Adwaith"]
+		},
+		{
+			
+			"Diamondat": "2.0",
+			"Cut": "Crown_facet",
+			"Colour": "Pink",
+			"Clarity": "VS1",
+			"Location":"abe" 
+			"Owners": ["Kollur","soham_industrial_diamonds","Beon_Group","Shah","Ajay_Gosh","Crayon_Bros_Ltd","Adora","Gaurav Singh"]
+		},
+		{
+			
+			"Diamondat": "2.0",
+			"Cut": "Crown_facet",
+			"Colour": "Blue",
+			"Clarity": "SI1",
+			"Location":"abf" 
+			"JewelleryType": "earrings",
+			"Owners": ["Kollur","soham_industrial_diamonds","Beon_Group","Shah","Ajay_Gosh","Crayon_Bros_Ltd","Adora","Adwaith"]
+		}
+	]
 };
 
-let full_scenario = {
-    'cars': [
-        {
-            'VIN': '720965981630055',
-            'Make': 'Toyota',
-            'Model': 'Yaris',
-            'Colour': 'Red',
-            'Reg': 'QD65 YKR',
-            'Owners': ['DVLA', 'Toyota', 'Beechvale Group', 'LeaseCan', 'Joe Payne', 'Cray Bros (London) Ltd']
-        },
-        {
-            'VIN': '287437467447767',
-            'Make': 'Toyota',
-            'Model': 'Auris',
-            'Colour': 'Blue',
-            'Reg': 'LM16 YHU',
-            'Owners': ['DVLA', 'Toyota', 'Beechvale Group', 'LeaseCan']
-        },
-        {
-            'VIN': '948881310167423',
-            'Make': 'Toyota',
-            'Model': 'Celica',
-            'Colour': 'Silver',
-            'Reg': 'DG16 FVG',
-            'Owners': ['DVLA', 'Toyota', 'Beechvale Group']
-        },
-        {
-            'VIN': '181255391772389',
-            'Make': 'Jaguar',
-            'Model': 'XJ',
-            'Colour': 'Black',
-            'Reg': 'FM65 ESL',
-            'Owners': ['DVLA', 'Jaguar Land Rover', 'Beechvale Group', 'LeaseCan']
-        },
-        {
-            'VIN': '549523556856725',
-            'Make': 'Jaguar',
-            'Model': 'F-Type',
-            'Colour': 'Red',
-            'Reg': 'HE16 WDZ',
-            'Owners': ['DVLA', 'Jaguar Land Rover', 'Beechvale Group']
-        },
-        {
-            'VIN': '523447019546831',
-            'Make': 'Land Rover',
-            'Model': 'Defender',
-            'Colour': 'Silver',
-            'Reg': 'EY16 FRV',
-            'Owners': ['DVLA', 'Jaguar Land Rover', 'Beechvale Group']
-        },
-        {
-            'VIN': '546303780997253',
-            'Make': 'Alfa Romeo',
-            'Model': 'Giulietta',
-            'Colour': 'White',
-            'Reg': 'JU65 XMH',
-            'Owners': ['DVLA', 'Alfa Romeo']
-        },
-        {
-            'VIN': '128994473011261',
-            'Make': 'Alfa Romeo',
-            'Model': 'MiTO',
-            'Colour': 'Black',
-            'Reg': 'YD65 FTB',
-            'Owners': ['DVLA', 'Alfa Romeo']
-        },
-        {
-            'VIN': '747542562791231',
-            'Make': 'Alfa Romeo',
-            'Model': '4C',
-            'Colour': 'Red',
-            'Reg': 'RX65 RNG',
-            'Owners': ['DVLA', 'Alfa Romeo']
-        },
-        {
-            'VIN': '880352730316924',
-            'Make': 'Alfa Romeo',
-            'Model': 'MiTo',
-            'Colour': 'Blue',
-            'Reg': 'NL65 DTU',
-            'Owners': ['DVLA', 'Alfa Romeo']
-        }
-    ]
+var full = {
+	"cars": [
+		{
+			
+			"Diamondat": "2.0",
+			"Cut": "Table",
+			"Colour": "Colourless",
+			"Clarity": "VVS2",
+			"Location":"abd" 
+			"Owners": ["Kollur","soham_industrial_diamonds","Beon_Group","Shah","Ajay_Gosh","Crayon_Bros_Ltd","Adora","Gaurav Singh"]
+		},
+		{
+			
+			"Diamondat": "5.0",
+			"Cut": "Point",
+			"Colour": "Blue",
+			"Clarity": "I1",
+			
+			"Location":"aba" 
+			"Owners": ["Kollur","soham_industrial_diamonds","Beon_Group","Shah","Ajay_Gosh","Crayon_Bros_Ltd","Adora","Amardev"]
+		},
+		{
+			
+			"Diamondat": "2.0",
+			"Cut": "mazarin",
+			"Colour": "Blue",
+			"Clarity": "VS1",
+			"Location":"abk" 
+			
+			"Owners": ["Kollur","soham_industrial_diamonds","Beon_Group","Shah","Ajay_Gosh","Crayon_Bros_Ltd","Adora","Adwaith"]
+		},
+		{
+			
+			"Diamondat": "1.0",
+			"Cut": "mazarin",
+			"Colour": "Pink",
+			"Clarity": "VS1",
+			"Location":"abm" 
+			"Owners": ["Kollur","soham_industrial_diamonds","Beon_Group","Shah","Ajay_Gosh","Crayon_Bros_Ltd","Adora","Amardev"]
+		},
+		{
+			
+			"Diamondat": "2.0",
+			"Cut": "Point",
+			"Colour": "Pink",
+			"Clarity": "VS2",
+			"Location":"abn" 
+			"Owners": ["Kollur","soham_industrial_diamonds","Beon_Group","Shah","Ajay_Gosh","Crayon_Bros_Ltd","Adora","Adwaith"]
+		},
+		{
+			
+			"Diamondat": "2.0",
+			"Cut": "Point",
+			"Colour": "Colourless",
+			"Clarity": "VVS1",
+			"Location":"abg" 
+			"Owners": ["Kollur","soham_industrial_diamonds","Beon_Group","Shah","Ajay_Gosh","Crayon_Bros_Ltd","Adora","Gaurav Singh"]
+		},
+		{
+			
+			"Diamondat": "5.0",
+			"Cut": "Point",
+			"Colour": "Blue",
+			"Clarity": "SI1",
+			"Location":"abh" 
+			"Owners": ["Kollur","soham_industrial_diamonds","Beon_Group","Shah","Ajay_Gosh","Crayon_Bros_Ltd","Adora","Gaurav Singh"]
+		},
+		{
+		
+			"Diamondat": "5.0",
+			"Cut": "mazarin",
+			"Colour": "Pink",
+			"Clarity": "SI2",
+			"Location":"aby" 
+			"Owners": ["Kollur","soham_industrial_diamonds","Beon_Group","Shah","Ajay_Gosh","Crayon_Bros_Ltd","Adora","Amardev"]
+		},
+		{
+			
+			"Diamondat": "2.0",
+			"Cut": "Table",
+			"Colour": "Blue",
+			"Clarity": "VS1",
+			"Location":"abt" 
+			"Owners": ["Kollur","soham_industrial_diamonds","Beon_Group","Shah","Ajay_Gosh","Crayon_Bros_Ltd","Adora","Adwaith"]
+		},
+		{
+			"Diamondat": "2.0",
+            "Cut": "Point",
+			"Colour": "Blue",
+			"Location":"abl"
+			"Owners": ["Kollur","soham_industrial_diamonds","Beon_Group","Shah","Ajay_Gosh","Crayon_Bros_Ltd","Adora","Gaurav Singh"]
+		}
+	]
 };
+
 
 
 exports.simple = simple_scenario;
