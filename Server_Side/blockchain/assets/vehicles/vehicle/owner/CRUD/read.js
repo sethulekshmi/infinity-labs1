d87265b21be0da7440da67043579add708a1f047
sethulekshmi <sethulekshmi777@gmail.com>
2017-03-07 23:11:39 +0530

    Update read.js

--- Server_Side/blockchain/assets/vehicles/vehicle/owner/CRUD/read.js
+++ Server_Side/blockchain/assets/vehicles/vehicle/owner/CRUD/read.js
@@ -3,5 +3,5 @@
 let read = require(__dirname+'/../../property/read');
 
 exports.read = function(req, res, next, usersToSecurityContext) {
-    return read(req, res, next, usersToSecurityContext, 'location');
+    return read(req, res, next, usersToSecurityContext, 'owner');
 };
