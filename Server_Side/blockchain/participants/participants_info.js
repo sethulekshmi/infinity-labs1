4fff78846e9c9e463bed1148f3b470ccf95b0df4
sethulekshmi <sethulekshmi777@gmail.com>
2017-03-07 22:11:34 +0530

    Update participants_info.js

--- Server_Side/blockchain/participants/participants_info.js
+++ Server_Side/blockchain/participants/participants_info.js
@@ -1,155 +1,159 @@
 'use strict';
 
 let participants_info = {
-	'regulators': [
-		{
-			'name': 'Kollur',
-			'identity': 'Kollur',
-			'password': 'IUZCYDngtwjW',
-			'address_line_1': 'kollur mine',
-			'address_line_2': 'Guntur',
-			'address_line_3': 'Andra Pradesh',
-			'address_line_4': 'India',
-			'postcode': 'SA7 0AN'
-		}
-	],
-	
-	'manufacturers': [
-		{
-			'name': 'Shah',
-			'identity': 'Shah',
-			'password': 'mRbbQTpZfVVa',
-			'address_line_1': 'Surat',
-			'address_line_2': 'Gujarat',
-			'address_line_3': 'India',
-			'postcode': 'SL82 4AB'
-		},
-		{
-			'name': 'Kothari',
-			'identity': 'Kothari',
-			'password': 'KakjewJfwBSq',
-			'address_line_1': 'Surat',
-			'address_line_2': 'Gujarat',
-			'address_line_3': 'India',
-			'postcode': 'WA18 7KJ'
-		},
-		{
-			'name': 'Agarwal',
-			'identity': 'Agarwal',
-			'password': 'plqOUyoFTZyK',
-			'address_line_1': 'Surat',
-			'address_line_2': 'Gujarat',
-			'address_line_3': 'India',
-			'postcode': 'M21 15QY'
-		}
-	],
-	'dealerships': [
-		{
-			'name': 'Ajay_Gosh',
-			'identity': 'Ajay_Gosh',
-			'password': 'BKwnxTfJGNyK',
-			'address_line_1': 'Mahadhipura',
-			'address_line_2': 'Gujarat',
-			'postcode': 'SO50 8JR'
-		},
-		{
-			'name': 'Andrew_Hurt',
-			'identity': 'Andrew_Hurt',
-			'password': 'tkGIRxBywwMk',
-			'address_line_1': 'Ahmedabad',
-			'address_line_2': 'Gujarat',
-			'postcode': 'SO50 9CL'
-		},
-		{
-			'name': 'Rahul_Ajay_Gandhi',
-			'identity': 'Rahul_Ajay_Gandhi',
-			'password': 'MrcAgjpBwhmI',
-			'address_line_1': 'Mahadhipura',
-			'address_line_2': 'Gujarat',
-			'postcode': 'SO50 3QV'
-		}
-	],
-	'lease_companies': [
-		{
-			'name': 'Crayon_Bros_Ltd',
-			'identity': 'Cray_Bros_London_Ltd',
-			'password': 'BTaWHtHrCZry',
-			'address_line_1': '26 Electric Avenue',
-			'address_line_2': 'Delhi',
-			'address_line_3': 'India',
-			'postcode': 'SE51 9DR'
-		},
-		{
-			'name': 'Aston_Cutting_Centre',
-			'identity': 'Aston_Cutting_Centre',
-			'password': 'AzdeAZuGtlUT',
-			'address_line_1': '11 Willow Park Way',
-			'address_line_2': 'Mumbai',
-			'address_line_3': 'India',
-			'postcode': 'DE72 2DG'
-		},
-		{
-			'name': 'ScrapIt',
-			'identity': 'ScrapIt',
-			'password': 'WDYJcenyScyC',
-			'address_line_1': '25 Layola Road',
-			'address_line_2': 'Mumbai',
-			'postcode': 'SO29 9BL'
-		}
-],
-'leasees': [
-		{
-			'name': 'Adora',
-			'identity': 'Adora',
-			'password': 'BTaWHtHrCZry',
-			'address_line_1': 'Mumbai',
-			'address_line_2': 'India',
-			'postcode': 'SE51 9DR'
-		},
-		{
-			'name': 'Tanishq',
-			'identity': 'Tanishq',
-			'password': 'AzdeAZuGtlUT',
-			'address_line_1': 'Mumbai',
-			'address_line_2': 'India',
-			'postcode': 'DE72 2DG'
-		},
-		{
-			'name': 'Kiah',
-			'identity': 'Kiah',
-			'password': 'WDYJcenyScyC',
-			'address_line_1': 'Mumbai',
-			'address_line_2': 'India',
-			'postcode': 'SO29 9BL'
-
-}
-],		'scrap_merchants': [
-		{
-			'name': 'Gaurav_Singh',
-			'identity': 'Gaurav_Singh',
-			'password': 'BTaWHtHrCZry',
-			'address_line_1': 'Mumbai',
-			'address_line_2': 'India',
-			'postcode': 'SE51 9DR'
-		},
-		{
-			'name': 'Adwaith',
-			'identity': 'Adwaith',
-			'password': 'AzdeAZuGtlUT',
-			'address_line_1': 'Mumbai',
-			'address_line_2': 'India',
-			'postcode': 'DE72 2DG'
-		},
-		{
-			'name': 'AmarDev',
-			'identity': 'Amar_Dev',
-			'password': 'WDYJcenyScyC',
-			'address_line_1': 'Mumbai',
-			'address_line_2': 'India',
-			'postcode': 'SO29 9BL'
-		}
-	]
-	
-
+    'regulators': [
+        {
+            'name': 'DVLA',
+            'identity': 'DVLA',
+            'password': 'IUZCYDngtwjW',
+            'address_line_1': 'The Richard Ley Development Centre',
+            'address_line_2': 'Upper Forest Way',
+            'address_line_3': 'Swansea Vale',
+            'address_line_4': 'Swansea',
+            'postcode': 'SA7 0AN'
+        }
+    ],
+    'manufacturers': [
+        {
+            'name': 'Alfa Romeo',
+            'identity': 'Alfa_Romeo',
+            'password': 'fGMMQqWEPxVy',
+            'address_line_1': '25 St James\'s Street',
+            'address_line_2': 'London',
+            'address_line_3': 'United Kingdom',
+            'postcode': 'SW1A 1HA'
+        },
+        {
+            'name': 'Toyota',
+            'identity': 'Toyota',
+            'password': 'hTpPFxiOwWgS',
+            'address_line_1': 'Burnaston',
+            'address_line_2': 'Derby',
+            'address_line_3': 'United Kingdom',
+            'postcode': 'DE1 9TA'
+        },
+        {
+            'name': 'Jaguar Land Rover',
+            'identity': 'Jaguar_Land_Rover',
+            'password': 'nNRyjPKrSpUb',
+            'address_line_1': 'Abbey Road',
+            'address_line_2': 'Coventry',
+            'address_line_3': 'United Kingdom',
+            'postcode': 'CV3 4LF'
+        }
+    ],
+    'dealerships': [
+        {
+            'name': 'Beechvale Group',
+            'identity': 'Beechvale_Group',
+            'password': 'TvNWKDgWTrfH',
+            'address_line_1': '84 Hull Road',
+            'address_line_2': 'Derby',
+            'postcode': 'DE75 4PJ'
+        },
+        {
+            'name': 'Milescape',
+            'identity': 'Milescape',
+            'password': 'cGJslZqjNjPK',
+            'address_line_1': 'Imperial Yard',
+            'address_line_2': 'Derby',
+            'postcode': 'DE94 8HY'
+        },
+        {
+            'name': 'Viewers Alfa Romeo',
+            'identity': 'Viewers_Alfa_Romeo',
+            'password': 'VWjFucyEIzTn',
+            'address_line_1': '25 Lower Lens Street',
+            'address_line_2': 'Penylan',
+            'address_line_3': 'Cardiff',
+            'address_line_4': 'South Glamorgan',
+            'postcode': 'CF28 9LC'
+        }
+    ],
+    'lease_companies': [
+        {
+            'name': 'LeaseCan',
+            'identity': 'LeaseCan',
+            'password': 'mRbbQTpZfVVa',
+            'address_line_1': '64 Zoo Lane',
+            'address_line_2': 'Slough',
+            'address_line_3': 'Berkshire',
+            'postcode': 'SL82 4AB'
+        },
+        {
+            'name': 'Every Car Leasing',
+            'identity': 'Every_Car_Leasing',
+            'password': 'KakjewJfwBSq',
+            'address_line_1': '9 Main Road',
+            'address_line_2': 'Mobberly',
+            'address_line_3': 'Cheshire',
+            'postcode': 'WA18 7KJ'
+        },
+        {
+            'name': 'Regionwide Vehicle Contracts',
+            'identity': 'Regionwide_Vehicle_Contracts',
+            'password': 'plqOUyoFTZyK',
+            'address_line_1': 'Unit 9',
+            'address_line_2': 'Malcom Christie Way',
+            'address_line_3': 'Riggot Fields',
+            'address_line_4': 'Manchester',
+            'postcode': 'M21 15QY'
+        }
+    ],
+    'leasees': [
+        {
+            'name': 'Joe Payne',
+            'identity': 'Joe_Payne',
+            'password': 'BKwnxTfJGNyK',
+            'address_line_1': '84 Byron Road',
+            'address_line_2': 'Eastleigh',
+            'postcode': 'SO50 8JR'
+        },
+        {
+            'name': 'Andrew Hurt',
+            'identity': 'Andrew_Hurt',
+            'password': 'tkGIRxBywwMk',
+            'address_line_1': '16 West Street',
+            'address_line_2': 'Eastleigh',
+            'postcode': 'SO50 9CL'
+        },
+        {
+            'name': 'Anthony O\'Dowd',
+            'identity': 'Anthony_O_Dowd',
+            'password': 'MrcAgjpBwhmI',
+            'address_line_1': '34 Main Road',
+            'address_line_2': 'Winchester',
+            'postcode': 'SO50 3QV'
+        }
+    ],
+    'scrap_merchants': [
+        {
+            'name': 'Cray Bros (London) Ltd',
+            'identity': 'Cray_Bros_London_Ltd',
+            'password': 'BTaWHtHrCZry',
+            'address_line_1': '26 Electric Eel Avenue',
+            'address_line_2': 'Twickenham',
+            'address_line_3': 'Greater London',
+            'postcode': 'SE51 9DR'
+        },
+        {
+            'name': 'Aston Scrap Centre',
+            'identity': 'Aston_Scrap_Centre',
+            'password': 'AzdeAZuGtlUT',
+            'address_line_1': '11 Willow Park Way',
+            'address_line_2': 'Aston on Trent',
+            'address_line_3': 'Derby',
+            'postcode': 'DE72 2DG'
+        },
+        {
+            'name': 'ScrapIt! UK',
+            'identity': 'ScrapIt_UK',
+            'password': 'WDYJcenyScyC',
+            'address_line_1': '25 Lincoln Road',
+            'address_line_2': 'Winchester',
+            'postcode': 'SO29 9BL'
+        }
+    ]
 };
+
 module.exports = participants_info;
