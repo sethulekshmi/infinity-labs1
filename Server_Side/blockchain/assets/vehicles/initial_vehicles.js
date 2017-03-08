e6b84d785cd14c89aca376a1188978616f69ca87
sethulekshmi <sethulekshmi777@gmail.com>
2017-03-08 17:43:01 +0530

    Update initial_vehicles.js

--- Server_Side/blockchain/assets/vehicles/initial_vehicles.js
+++ Server_Side/blockchain/assets/vehicles/initial_vehicles.js
@@ -3,7 +3,7 @@ let simple_scenario = {
     'cars': [
         {
             
-			"Diamondat": "2.0",
+			"Diamondat": "287437467447767",
 			"Cut": "Point",
 			"Colour": "Blue",
 			"Clarity": "VVS1",
@@ -14,7 +14,7 @@ let simple_scenario = {
 		},
 		{
 			
-			"Diamondat": "2.0",
+			"Diamondat": "287437467447767",
 			"Cut": "Crown_facet",
 			"Colour": "Pink",
 			"Clarity": "VS1",
@@ -23,7 +23,7 @@ let simple_scenario = {
 		},
 		{
 			
-			"Diamondat": "2.0",
+			"Diamondat": "287437467447767",
 			"Cut": "Crown_facet",
 			"Colour": "Blue",
 			"Clarity": "SI1",
@@ -38,7 +38,7 @@ var full = {
 	"cars": [
 		{
 			
-			"Diamondat": "2.0",
+			"Diamondat": "287437467447767",
 			"Cut": "Table",
 			"Colour": "Colourless",
 			"Clarity": "VVS2",
@@ -47,7 +47,7 @@ var full = {
 		},
 		{
 			
-			"Diamondat": "5.0",
+			"Diamondat": "287437467447765",
 			"Cut": "Point",
 			"Colour": "Blue",
 			"Clarity": "I1",
@@ -57,7 +57,7 @@ var full = {
 		},
 		{
 			
-			"Diamondat": "2.0",
+			"Diamondat": "287437467447767",
 			"Cut": "mazarin",
 			"Colour": "Blue",
 			"Clarity": "VS1",
@@ -67,7 +67,7 @@ var full = {
 		},
 		{
 			
-			"Diamondat": "1.0",
+			"Diamondat": "287437467447766",
 			"Cut": "mazarin",
 			"Colour": "Pink",
 			"Clarity": "VS1",
@@ -76,7 +76,7 @@ var full = {
 		},
 		{
 			
-			"Diamondat": "2.0",
+			"Diamondat": "287437467447767",
 			"Cut": "Point",
 			"Colour": "Pink",
 			"Clarity": "VS2",
@@ -85,7 +85,7 @@ var full = {
 		},
 		{
 			
-			"Diamondat": "2.0",
+			"Diamondat": "287437467447767",
 			"Cut": "Point",
 			"Colour": "Colourless",
 			"Clarity": "VVS1",
@@ -94,7 +94,7 @@ var full = {
 		},
 		{
 			
-			"Diamondat": "5.0",
+			"Diamondat": "287437467447765",
 			"Cut": "Point",
 			"Colour": "Blue",
 			"Clarity": "SI1",
@@ -103,7 +103,7 @@ var full = {
 		},
 		{
 		
-			"Diamondat": "5.0",
+			"Diamondat": "287437467447765",
 			"Cut": "mazarin",
 			"Colour": "Pink",
 			"Clarity": "SI2",
@@ -112,7 +112,7 @@ var full = {
 		},
 		{
 			
-			"Diamondat": "2.0",
+			"Diamondat": "287437467447767",
 			"Cut": "Table",
 			"Colour": "Blue",
 			"Clarity": "VS1",
@@ -120,7 +120,7 @@ var full = {
 			"Owners": ["Kollur","soham_industrial_diamonds","Beon_Group","Shah","Ajay_Gosh","Crayon_Bros_Ltd","Adora","Adwaith"]
 		},
 		{
-			"Diamondat": "2.0",
+			"Diamondat": "287437467447767",
             "Cut": "Point",
 			"Colour": "Blue",
 			"Location":"abl"
